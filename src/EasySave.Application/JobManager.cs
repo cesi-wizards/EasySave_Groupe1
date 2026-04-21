756d5e4f7ec5f7b31f79def7924db2deb26ee4af
robin <robin.derenty@gmail.com>
2026-04-21 15:51:55 +0200

    feat: implement JobManager with job addition, removal, and execution functionality

--- src/EasySave.Application/JobManager.cs
+++ src/EasySave.Application/JobManager.cs
@@ -1,6 +1,45 @@
+using EasySave.Domain.Entities;
+using EasySave.Infrastructure.Factories;
+using EasySave.Infrastructure.Factories.Interfaces;
+
 namespace EasySave.Application;
 
 public class JobManager
 {
+    public List<BackupJob> Jobs { get; set; } = [];
 
+    public void AddJob(BackupConfig config)
+    {
+        IBackupFactory backupFactory;
+
+        if (config.Type == BackupType.Full)
+        {
+            backupFactory = new FullBackupFactory();
+        }
+        else
+        {
+            backupFactory = new DifferentialBackupFactory();
+        }
+
+        BackupJob jobToAdd = backupFactory.CreateJob(config.Name, config.SourcePath, config.TargetPath);
+        Jobs.Add(jobToAdd);
+    }
+
+    public void RemoveJob(string backupName)
+    {
+        var jobToRemove = Jobs.Find(job => job.Name == backupName);
+        if (jobToRemove != null)
+        {
+            Jobs.Remove(jobToRemove);
+        }
+    }
+
+    public void ExecuteJobs()
+    {
+        foreach (BackupJob job in Jobs)
+        {
+            job.Execute();
+        }
+
+    }
 }
