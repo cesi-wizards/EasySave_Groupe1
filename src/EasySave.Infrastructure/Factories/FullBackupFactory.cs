e43eb1121b4937c5d9dbdbde181e2a29fea88f59
robin <robin.derenty@gmail.com>
2026-04-21 15:50:53 +0200

    feat: add Backup factories

--- src/EasySave.Infrastructure/Factories/FullBackupFactory.cs
+++ src/EasySave.Infrastructure/Factories/FullBackupFactory.cs
@@ -1,6 +1,14 @@
+using EasySave.Domain.Entities;
+using EasySave.Domain.Interfaces;
+using EasySave.Domain.Strategies;
+
 namespace EasySave.Infrastructure.Factories;
 
-public class FullBackupFactory
+public class FullBackupFactory : AbstractBackupFactory
 {
-
+    public override BackupJob CreateJob(string jobName, string srcPath, string targetPath)
+    {
+        IBackupStrategy strategy = new FullBackupStrategy();
+        return CreateJobWithStrategy(jobName, srcPath, targetPath, strategy);
+    }
 }
