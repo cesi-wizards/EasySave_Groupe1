b8c144527d758bd66e7217e818ae852eee1689e0
robin <robin.derenty@gmail.com>
2026-04-21 15:49:55 +0200

    feat: add BackupConfig

--- src/EasySave.Domain/Entities/BackupConfig.cs
+++ src/EasySave.Domain/Entities/BackupConfig.cs
@@ -2,5 +2,8 @@ namespace EasySave.Domain.Entities;
 
 public class BackupConfig
 {
-
+    public required string Name { get; set; }
+    public required string SourcePath { get; set; }
+    public required string TargetPath { get; set; }
+    public required BackupType Type { get; set; }
 }
