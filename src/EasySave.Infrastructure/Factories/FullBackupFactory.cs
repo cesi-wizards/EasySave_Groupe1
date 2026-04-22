46f808ce41826706880079c42c974952a2f41e6e
robin <robin.derenty@gmail.com>
2026-04-22 11:23:20 +0200

    fix: add primary constructor to factories to add global subscribers instanciated by JobManager

--- src/EasySave.Infrastructure/Factories/FullBackupFactory.cs
+++ src/EasySave.Infrastructure/Factories/FullBackupFactory.cs
@@ -1,9 +1,10 @@
 using EasySave.Domain.Entities;
+using EasySave.Domain.Interfaces;
 using EasySave.Domain.Strategies;
 
 namespace EasySave.Infrastructure.Factories;
 
-public class FullBackupFactory : AbstractBackupFactory
+public class FullBackupFactory(List<ISubscriber> subscribers) : AbstractBackupFactory(subscribers)
 {
     public override BackupJob CreateJob(string jobName, string srcPath, string targetPath)
     {
