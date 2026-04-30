026bf0bb0d591c3c7773641b7e758aac187904be
Antoine Von Tokarski <antoine.vontokarski@viacesi.fr>
2026-04-30 14:11:32 +0200

    fix: use of using Process so as to call IDisposable, awaiting review comments

--- src/EasySave.Domain/Façade/CryptoSoftService.cs
+++ src/EasySave.Domain/Façade/CryptoSoftService.cs
@@ -5,23 +5,30 @@ namespace EasySave.Domain.Façade;
 public class CryptoSoftService
 {
     private static readonly string _cryptoSoftExcutableName = "CryptoSoft.exe";
-
     public static int Encrypt(string filePath, string key)
     {
-        Process process = new Process
+        using (Process process = new Process())
         {
-            StartInfo = new ProcessStartInfo
+            process.StartInfo = new ProcessStartInfo
             {
                 FileName = _cryptoSoftExcutableName,
-                Arguments = $"\"{filePath}\" {key}",
+                Arguments = $"\"{filePath}\" \"{key}\"",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
-            }
-        };
+            };
+            try
+            {
+                process.Start();
+                process.WaitForExit();
 
-        process.Start();
-        process.WaitForExit();
-        return process.ExitCode;
+                return process.ExitCode;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error CryptoSoft : {ex.Message}");
+                return -1;
+            }
+        }
     }
 }
