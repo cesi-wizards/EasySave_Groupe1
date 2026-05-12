86291e37e5e4b86222119c119d6b72349ae18c8d
robin <robin.derenty@gmail.com>
2026-05-12 09:36:57 +0200

    feat: add ErrorDialog view and view-model

--- src/EasySave.GUI/App.axaml.cs
+++ src/EasySave.GUI/App.axaml.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Linq;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using EasySave.GUI.Services;
 using EasySave.GUI.ViewModels;
 using EasySave.GUI.Views;
@@ -29,6 +32,45 @@ public partial class App : Avalonia.Application
             };
         }
 
+        RegisterGlobalExceptionHandlers();
+
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void RegisterGlobalExceptionHandlers()
+    {
+        // Unhandled exceptions on the UI thread (Avalonia dispatcher)
+        Dispatcher.UIThread.UnhandledException += (_, e) =>
+        {
+            e.Handled = true;
+            ShowErrorDialog(e.Exception);
+        };
+
+        // Unhandled exceptions on background threads
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is Exception ex)
+                Dispatcher.UIThread.Post(() => ShowErrorDialog(ex));
+        };
+
+        // Unobserved Task exceptions
+        TaskScheduler.UnobservedTaskException += (_, e) =>
+        {
+            e.SetObserved();
+            Dispatcher.UIThread.Post(() => ShowErrorDialog(e.Exception));
+        };
+    }
+
+    public static void ShowErrorDialog(Exception ex)
+    {
+        var owner = (Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)
+            ?.MainWindow;
+
+        var dialog = new ErrorDialog(ex);
+
+        if (owner is not null)
+            dialog.ShowDialog(owner);
+        else
+            dialog.Show();
     }
 }
